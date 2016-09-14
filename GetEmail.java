48d6f90ad37dfd5ae0619694c8f388f0bd6292d5
Stavros Papapantelis <lucid.gr@gmail.com>
2016-09-14 23:58:01 +0300

    Update GetEmail.java

--- GetEmail.java
+++ GetEmail.java
@@ -7,22 +7,22 @@ public class GetEmail {
 	public static void main(String[] args) {
 
 		String RandomText = "asdf!>:<email@gmail.gr:..03asdf";
-		
-		LinkedList<String> correctEmails = new LinkedList<String>();
-		
+
+                breakpoint:
 		for (int i=0; i<RandomText.length()-10; i++){
 			for (int j=RandomText.length(); j>4+i; j--){
-				if(getEmail(RandomText.substring(i,j))){
-				correctEmails.add(RandomText.substring(i,j));
+				if(isEmail(RandomText.substring(i,j))){
+				System.out.println("Correct Email: " + RandomText.substring(i,j));
+				break breakpoint;
 				}
 			}
 		}
-		System.out.println("Correct Email: " + correctEmails.getFirst());
+	 
    
      }
 		
 	 
-	 private static Boolean getEmail(String email){
+	 private static Boolean isEmail(String email){
 	 Pattern p = Pattern.compile("\\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b", Pattern.CASE_INSENSITIVE);
 	 Matcher matcher = p.matcher(email);
            matcher.find();
